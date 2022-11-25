c5ed7711b41093ea63ee47c0c52babb17aba5f05
Charlie <charlottevfarmer@gmail.com>
2022-11-25 11:42:21 +0000

    Create PostDetail view & wireup and add starter html for later editing

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     # url for index.html template view
     path('', views.PostList.as_view(), name='home'),
+    # url for post_details.html template to view full post info
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
